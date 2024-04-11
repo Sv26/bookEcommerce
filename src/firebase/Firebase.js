cecd3de38d4312f9ee66297f516489bda94e0bfc
Pankaj Verma <pankajverma@Pankajs-MacBook-Pro.local>
2024-04-11 13:26:48 +0530

    first commit

---
+++ src/firebase/Firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrTFoT0opXXaA7UpagllFJkjNozWUrV04",
+  authDomain: "boooksite-59192.firebaseapp.com",
+  projectId: "boooksite-59192",
+  storageBucket: "boooksite-59192.appspot.com",
+  messagingSenderId: "1097242320221",
+  appId: "1:1097242320221:web:24b363fa988d7e36a91cb4",
+  measurementId: "G-VP3EYER1QD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
